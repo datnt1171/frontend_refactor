32887e53c3072c699ad0fda71866125b7a415abd
datnt1171 <datnt1171@gmail.com>
2025-06-21 09:18:21 +0700

    chore: change default to 'vi'

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from "@/i18n/navigation"
 
 export default function Home() {
   // Redirect to login page
-  redirect({ href: "/login", locale: "en" })
+  redirect({ href: "/login", locale: 'vi' })
 
   // This won't be rendered, but we include it for completeness
   return null
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,8 +2,8 @@ import {defineRouting} from 'next-intl/routing';
  
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['en', 'vi', 'zh-hant'],
+  locales: ['vi', 'en', 'zh-hant'],
  
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: 'vi'
 });
