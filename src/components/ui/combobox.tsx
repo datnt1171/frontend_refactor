f332787d4e8701f36ffabf30e81a55d1caa1f788
datnt1171 <datnt1171@gmail.com>
2025-08-25 21:46:02 +0700

    fix: force popover to the bottom

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -59,7 +59,7 @@ export function Combobox({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-full p-0" align="start">
+      <PopoverContent className="w-full p-0" align="start" side="bottom" avoidCollisions={false}>
         <Command>
           <CommandInput placeholder={searchPlaceholder} />
           <CommandEmpty>{emptyMessage}</CommandEmpty>
