ca98d676a65c56ed1e6c7bbb596fd5c3979268d7
datnt1171 <datnt1171@gmail.com>
2025-10-10 13:20:05 +0700

    feat: add month-range filter

--- src/lib/utils/filter.ts
+++ src/lib/utils/filter.ts
@@ -62,3 +62,18 @@ export function getDepartmentOptions() {
     { value: 'TT', label: 'TT' },
   ];
 }
+
+export const MONTH_OPTIONS = [
+  { value: '1', label: 'January' },
+  { value: '2', label: 'February' },
+  { value: '3', label: 'March' },
+  { value: '4', label: 'April' },
+  { value: '5', label: 'May' },
+  { value: '6', label: 'June' },
+  { value: '7', label: 'July' },
+  { value: '8', label: 'August' },
+  { value: '9', label: 'September' },
+  { value: '10', label: 'October' },
+  { value: '11', label: 'November' },
+  { value: '12', label: 'December' }
+];
